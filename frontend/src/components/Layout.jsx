adf363a1226615997e082581d1e341fadce8483e
FahroziAldinata <fahrozialdinata22s03@gmail.com>
2026-07-07 00:20:52 +0700

    chore(layout): reorder side navigation links for AKUNTAN role

--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -35,7 +35,7 @@ export const Layout = () => {
               {user?.role === 'AKUNTAN' && (
                 <>
                   <li>
-                    <Link to="/akuntan">RAB Harian</Link>
+                    <Link to="/akuntan/laporan/periode-setup">Setup Periode</Link>
                   </li>
                   <li>
                     <Link to="/akuntan/laporan">Laporan</Link>
@@ -47,13 +47,13 @@ export const Layout = () => {
                     <Link to="/akuntan/laporan/kebutuhan-belanja-bahan">Belanja Bahan</Link>
                   </li>
                   <li>
+                    <Link to="/akuntan">RAB Harian</Link>
+                  </li>
+                  <li>
                     <Link to="/akuntan/laporan/per-periode">Per Periode</Link>
                   </li>
                   <li>
                     <Link to="/akuntan/laporan/per-bulan">Per Bulan</Link>
-                  </li>
-                  <li>
-                    <Link to="/akuntan/laporan/periode-setup">Setup Periode</Link>
                   </li>
                 </>
               )}
